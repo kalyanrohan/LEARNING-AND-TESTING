bedfb0dbffff9bf919cca5125e07c7df3f20a20d
kalyanrohan <71363651+kalyanrohan@users.noreply.github.com>
2020-09-20 19:56:43 +0700

    dictionaries
    learnt how dictioanry works

--- TEST 1.py
+++ TEST 1.py
@@ -17,7 +17,87 @@ def function_2():
         return("even")
     else:
         return("odd")
-print(function_2())
+
+#how dictionary works, ditionary uses {}
+#essentially it uses keys to indetify certain values
+Months= {"Jan":"Januray",
+"Feb":"February",
+"Mar":"March",
+"Apr":"April",
+"May":"May",
+"Jun":"June",
+"Jul":"July",
+"Aug":"August",
+'Sept':"September",
+"Oct":"October",
+"Nov":"November",
+"Dec":"December"
+}
+#to print all of the keys in the dcitionary
+for x in Months:
+    print(x)
+#to print the values of the keys in the dictionary
+for x in Months:
+    print(Months[x])
+#another way to print the values of the keys in the dictionary
+for x in Months.values():
+    print(x)
+#If want to print both values and keys
+for x in Months.items():
+    print(x)
+# you can also check whether a key exist in a dictionary
+if "Jan" in Months:
+    print("January is there")
+# to change the values in the dictionary
+Months["Jan"]="janice"
+print(Months["Jan"])
+# to add new items to the dictionary
+Months["year"]=2020
+# another way to print keys and their corresponding items
+for x,y in Months.items():
+    print(x,y)
+# to remove an item from the dictionary
+Months.pop("Feb")
+# to remove the last item in the dictionary
+Months.popitem()
+print(Months)
+# to copy a dicitionary
+months2= Months.copy()
+months3=dict(Months)
+# creating dictionaries in a dictionary
+Patients= {
+    "Patient 1":{"Name":"Marcus","Age":21},
+    "Patient 2":{"Name":"Joan","Age":24},
+    "Patient 3":{"Name":"Gabriel","Age":20}
+}
+# To get the value of the value
+print(Patients.get("Patient 2").get("Age"))
+# Alternatively
+Patient_1={"Name":"Marcus","Age":21}
+Patient_2={"Name":"Joan","Age":24}
+Patient_3={"Name":"Gabriel","Age":20}
+Pateints={"Patient 1":Patient_1,"Patient 2":Patient_2,"Patient 3":Patient_3}
+print(Patients.get("Patient 2").get("Age"))
+#Alternate way to create dictionaries
+Colors=dict(light="Yellow",dark="Black",mixed="Green")
+print(Colors["light"])
+#to update your dictionary
+Colors.update({"light":"grey"})
+print(Colors)
+#to check whether a key exists. If key don't exist, can add it as a new key.
+Colors.setdefault("light","ash grey")
+print(Colors)
+
+
+
+
+
+
+
+
+
+
+
 
 
 
