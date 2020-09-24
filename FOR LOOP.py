86953f257636479751dc5cba49271661e9f97c14
kalyanrohan <71363651+kalyanrohan@users.noreply.github.com>
2020-09-24 13:45:09 +0700

    CREATED FOR LOOP FILE

---
+++ FOR LOOP.py
@@ -0,0 +1,15 @@
+colours= ["red","blue","green"]
+for x in colours:
+    print(x)
+for x in range(0,10):
+    print(x)
+# we can use the for loop to get the average of numbers in an array
+numbers= [23,45,21,32,45,21,33,47,19]
+#number is equal to the value of numbers
+for number in numbers:
+    start=0
+    number=start+number
+    print(number)
+
+
+    
