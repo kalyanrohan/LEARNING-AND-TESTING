3ddb743489020498d02897c31823513235aec2c7
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-08 17:42:54 +0700

    dicitionary excercise

---
+++ DICTIONARYEXCERCISE.py
@@ -0,0 +1,7 @@
+# user_input= input("phone: ")
+# numbers={"1":"one","2":"two","3":"three","4":"four","5":"five","6":"six","7":"seven","8":"eight","9":"nine","0":"zero"}
+# output=""
+# for x in user_input:
+#     output+=numbers.get(x, "!")+ " "
+# print(output)
+
