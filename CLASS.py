a1a613ca4d8407b0d01d813bd059e214c92ed3ff
kalyanrohan <sri.rohan@binus.ac.id>
2020-10-25 19:20:10 +0700

    update1

--- CLASS.py
+++ CLASS.py
@@ -20,4 +20,25 @@ print("Woo is also a {}".format(woo.__class__.species))
 # access the instance attributes
 print("{} is {} years old".format( blu.name, blu.age))
 print("{} is {} years old".format( woo.name, woo.age))
-print(blu.__class__.species)
+
+class Parrot:
+    
+    # instance attributes
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    # instance method
+    def sing(self, song):
+        return "{} sings {}".format(self.name, song)
+
+    def dance(self):
+        return "{} is now dancing".format(self.name)
+
+# instantiate the object
+blu = Parrot("Blu", 10)
+
+# call our instance methods
+print(blu.sing("'Happy'"))
+print(blu.dance()
+
---
+++ sets.py
@@ -0,0 +1,6 @@
+set1= {1,2,3}
+set2={2,3,4}
+set3={3,4,5}
+print(set2 and set1.difference(set3))
+print(set1.difference(set2,set3))
+
